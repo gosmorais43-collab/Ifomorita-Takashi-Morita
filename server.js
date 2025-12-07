00ea312bfc01109d9bf4c7039318a88d8a7722d6
gosmorais43-collab <gosmorais.43@gmail.com>
2025-12-06 21:12:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ✅ CORS primeiro
 app.use(cors({
-  origin: 'https://tfomorita.onrender.com'
+  origin: 'https://ifomorita.onrender.com'
 }));
 
 // ✅ JSON parser
@@ -149,6 +149,7 @@ app.post('/reset-password-by-rm-cpf', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 
 
 
